9037b261f05b13f9131e5ee33f69969b3d23262d
Pedro Ayres <pedroayresb@gmail.com>
2022-08-02 19:48:43 -0300

    Realiza requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 export default class Search extends Component {
   constructor() {
     super();
     this.state = {
       isSaveButtonDisabled: true,
+      loading: false,
+      artistAlbums: [],
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -21,16 +27,31 @@ export default class Search extends Component {
     }
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+    const { value } = event.target.parentNode.getElementsByTagName('input')[0];
+    this.setState({ artist: ` ${value}` }, () => {
+      event.target.parentNode.getElementsByTagName('input')[0].value = '';
+      this.setState({ loading: true }, async () => {
+        const artistAlbums = await searchAlbumsAPI(value);
+        this.setState({ artistAlbums }, () => {
+          this.setState({ loading: false });
+        });
+      });
+    });
+  }
+
   render() {
     const { username } = this.props;
-    const { isSaveButtonDisabled } = this.state;
+    const { isSaveButtonDisabled, loading, artistAlbums, artist } = this.state;
+
     return (
       <div>
         <Header username={ username } />
         <div data-testid="page-search">
           <form>
             <label htmlFor="artist">
-              Digite o(s) nome(s) do(s) artista(s)
+              Digite o nome do artista:
               <input
                 type="text"
                 data-testid="search-artist-input"
@@ -39,6 +60,7 @@ export default class Search extends Component {
               <button
                 type="button"
                 data-testid="search-artist-button"
+                onClick={ this.onSubmit }
                 disabled={ isSaveButtonDisabled }
               >
                 Pesquisar
@@ -46,10 +68,42 @@ export default class Search extends Component {
             </label>
           </form>
         </div>
+        <div>
+          {loading && <Loading />}
+          {!loading && artistAlbums.length > 0 && (
+            <div className="results-container">
+              <p>
+                Resultado de álbuns de:
+                {artist}
+              </p>
+              <div className="results">
+                {artistAlbums.map((album, i) => (
+                  <div key={ i } className="result">
+                    <Link
+                      to={ `/album/${album.collectionId}` }
+                      data-testid={ `link-to-album-${album.collectionId}` }
+                    >
+                      <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                      <p>{ album.collectionName }</p>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          {!loading && artistAlbums.length === 0 && (
+            <div className="results-container">
+              <p>
+                Nenhum álbum foi encontrado
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
 }
+
 Search.propTypes = {
   username: propTypes.string.isRequired,
 };
