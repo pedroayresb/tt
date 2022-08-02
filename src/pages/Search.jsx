dc0d1bd7168d7f41c9e4604d15702f1a47bbbeb2
Pedro Ayres <pedroayresb@gmail.com>
2022-08-02 18:26:57 -0300

    Realiza requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,19 +3,53 @@ import propTypes from 'prop-types';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isSaveButtonDisabled: true,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    const { value } = event.target;
+    const minLength = 2;
+    if (value.length >= minLength) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  }
+
   render() {
     const { username } = this.props;
+    const { isSaveButtonDisabled } = this.state;
     return (
       <div>
         <Header username={ username } />
         <div data-testid="page-search">
-          <h1>Search</h1>
+          <form>
+            <label htmlFor="artist">
+              Digite o(s) nome(s) do(s) artista(s)
+              <input
+                type="text"
+                data-testid="search-artist-input"
+                onChange={ this.onInputChange }
+              />
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ isSaveButtonDisabled }
+              >
+                Pesquisar
+              </button>
+            </label>
+          </form>
         </div>
       </div>
     );
   }
 }
-
 Search.propTypes = {
   username: propTypes.string.isRequired,
 };
